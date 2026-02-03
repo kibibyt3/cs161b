abfab6ac9572a414e70660f092bdfb4de8c379e3
Liv Haze <kibibyt3@proton.me>
2026-02-02 18:53:49 -0800

    number_shifter: add test case and documentation

--- number_shifter/src/main.cpp
+++ number_shifter/src/main.cpp
@@ -33,7 +33,7 @@ int main() {
    char confirm_char = '\0';
    int *nums = NULL;
 
-   //tests();
+   tests();
 
    cout << "This program will shift a list of numbers to the right." << endl;
    do {
@@ -106,19 +106,51 @@ void shift_nums(int arr[], size_t count, size_t shift) {
    size_t greatest_common_divisor = 0;
    size_t i = 0;
 
-   
+   // By using a non-naive implementation, we bring the time complexity from
+   // O(n^2) down to O(n) (unless gcd is implemented very poorly.)
+   // The GCD of count and shift decide how many unique, neighboring "chains"
+   // of numbers we have in arr. From there, we just shift each chain the
+   // requisite amount. In this way we only move each element in arr once,
+   // while also keeping a small memory footprint.
+   // Here's a demonstration with shift = 3 and GCD = 3.
+   //  ______________________________________________
+   // |    |    |    |    |    |    |     |     |    |
+   // | x1 | x2 | x3 | x4 | x5 | x6 | x7  | ... | xn |
+   // |    |    |    |    |    |    |     |     |    |
+   // ------------------------------------------------
+   //   |              |              |
+   // --.              |              |
+   //     .-------------  width = GCD |
+   //     |             .--------------    
+   //     |             |              .---- ...
+   //     v             v              v
+   //  ______________________________________________
+   // |    |    |    |    |    |    |     |     |    |
+   // | x4 | x2 | x3 | x7 | x5 | x6 | x10 | ... | xn |
+   // |    |    |    |    |    |    |     |     |    |
+   // ------------------------------------------------
+   //
+   // We repeat the above process as many times as needed to bridge the
+   // distance of shift. In the above example, we'd repeat 2 more times.
    greatest_common_divisor = gcd(count, shift);
    for (i = 0; i < greatest_common_divisor; i++) {
       shift_chain(arr, count, shift, i);
    }
 }
 
+// Name:    shift_chain(int arr[], size_t count, size_t shift, size_t start)
+// Desc:    Shift one "chain" of numbers in an array.
+// Input:   int array, size_t (array count), size_t (shift degree)
+//          size_t (chain's start index)
+// Output:  None
+// Return:  None
 void shift_chain(int arr[], size_t count, size_t shift, size_t start) {
    int temp_right = 0;
    int temp_left = 0;
    size_t i = 0;
 
    temp_right = arr[start];
+   // DEBUG: change this to "i = start + count * n" where n is some number
    for (i = start + count - shift; i > start; i -= shift) {
       temp_left = arr[i];
       arr[i] = temp_right;
@@ -132,15 +164,23 @@ void tests() {
    int arr_chain_expected[] = {3, 2, 5, 4, 1, 6};
    int arr[] = {1, 2, 3, 4, 5, 6};
    int arr_expected[] = {4, 5, 6, 1, 2, 3};
+   int arr_weird[] = {1, 2, 3, 4, 5, 6};
+   int arr_weird_expected[] = {5, 6, 1, 2, 3, 4};
    int i = 0;
 
    shift_chain(arr_chain, 6, 2, 0);
-   for (i = 0; i < 6; i += 1) {
+   for (i = 0; i < 6; i++) {
       assert(arr_chain[i] == arr_chain_expected[i]);
    }
    
    shift_nums(arr, 6, 3);
-   for (i = 0; i < 6; i += 1) {
+   for (i = 0; i < 6; i++) {
       assert(arr[i] == arr_expected[i]);
    }
+
+   shift_nums(arr, 6, 4);
+   for (i = 0; i < 6; i++) {
+      assert(arr_weird[i] == arr_weird_expected[i]);
+   }
+
 }
