a1822a718f9718bacb92c718991b6b902cc22493
Liv Haze <kibibyt3@proton.me>
2026-02-03 17:06:27 -0800

    number_shifter: fix bugs and documentation

--- number_shifter/src/main.cpp
+++ number_shifter/src/main.cpp
@@ -1,11 +1,12 @@
 /******************************************************************************
-# Author:
-# Assignment:
-# Date:
-# Description:
-# Input:
-# Output:
-# Sources:
+# Author:      Liv Callister
+# Assignment:  Discussion 4
+# Date:        3 February 2026
+# Description: This program shifts a list of numbers to the right.
+# Input:       size_t (amount of numbers), int arr[] (list of numbers),
+#              size_t (amount of shifts)
+# Output:      int arr[] (list of shifted numbers)
+# Sources:     en.cppreference.com, Discussion 4 Specifications
 #*****************************************************************************/
 #include <iostream>
 #include <numeric>
@@ -21,11 +22,11 @@ void tests();
 
 const size_t MAX_RECOMMENDED_NUM_COUNT = 100;
 
-// Name:
-// Desc:
-// Input:
-// Output:
-// Return:
+// Name:    int main()
+// Desc:    This function controls the high-level control of the program.
+// Input:   None
+// Output:  None
+// Return:  0 on a successful execution
 int main() {
    size_t num_count = 0;
    size_t shift = 0;
@@ -33,7 +34,8 @@ int main() {
    char confirm_char = '\0';
    int *nums = NULL;
 
-   tests();
+   // Uncomment this line to run unit tests.
+   // tests();
 
    cout << "This program will shift a list of numbers to the right." << endl;
    do {
@@ -43,7 +45,7 @@ int main() {
       cin >> num_count;
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
       if (num_count > MAX_RECOMMENDED_NUM_COUNT) {
-         cout << "Allocating this much memory may not be possible." << endl;
+         cout << "Note: Allocating this much memory may not be possible." << endl;
          cout << "Are you sure you want to create a list with ";
          cout << num_count << " numbers? (y/N)" << endl;
          cout << ">> ";
@@ -82,14 +84,19 @@ int main() {
          && (confirm_char != 'y' && confirm_char != 'Y'));
 
    // There are only num_count many possible permutations.
-   shift %= num_count;
+   shift = (num_count == 0) ? 0 : shift % num_count;
 
    shift_nums(nums, num_count, shift);
    cout << "Here are your shifted numbers:" << endl;
-   for (i = 0; i < num_count - 1; i++) {
-      cout << nums[i] << ", ";
+   if (num_count > 0) {
+      for (i = 0; i < num_count - 1; i++) {
+         cout << nums[i] << ", ";
+      }
+      cout << nums[num_count - 1] << '.' << endl;
    }
-   cout << nums[num_count - 1] << '.' << endl;
+   else {
+      cout << "Your list was empty!" << endl;
+   }
    
    delete[] nums;
 
@@ -107,7 +114,7 @@ void shift_nums(int arr[], size_t count, size_t shift) {
    size_t i = 0;
 
    // By using a non-naive implementation, we bring the time complexity from
-   // O(n^2) down to O(n) (unless gcd is implemented very poorly.)
+   // O(n^2) down to O(n), where n = count (unless gcd is implemented poorly.)
    // The GCD of count and shift decide how many unique, neighboring "chains"
    // of numbers we have in arr. From there, we just shift each chain the
    // requisite amount. In this way we only move each element in arr once,
@@ -130,8 +137,9 @@ void shift_nums(int arr[], size_t count, size_t shift) {
    // |    |    |    |    |    |    |     |     |    |
    // ------------------------------------------------
    //
-   // We repeat the above process as many times as needed to bridge the
-   // distance of shift. In the above example, we'd repeat 2 more times.
+   // We repeat the above process as many times as needed to shift every value.
+   // In the above example, we'd repeat 2 more times, beginning on x2 and then
+   // on x3.
    greatest_common_divisor = gcd(count, shift);
    for (i = 0; i < greatest_common_divisor; i++) {
       shift_chain(arr, count, shift, i);
@@ -148,10 +156,19 @@ void shift_chain(int arr[], size_t count, size_t shift, size_t start) {
    int temp_right = 0;
    int temp_left = 0;
    size_t i = 0;
+   size_t distance = 0;
 
    temp_right = arr[start];
-   // DEBUG: change this to "i = start + count * n" where n is some number
-   for (i = start + count - shift; i > start; i -= shift) {
+   for (
+         // distance must never be < 0, and is thus set to a theoretical
+         // maximum. This value could be lower, but we'd need to know the value
+         // of gcd(count, shift).
+         distance = start + (count * shift) - shift;
+         distance % count != start;
+         distance -= shift
+   ) {
+      // distance % count is the actual index we work with.
+      i = distance % count;
       temp_left = arr[i];
       arr[i] = temp_right;
       temp_right = temp_left;
@@ -159,6 +176,12 @@ void shift_chain(int arr[], size_t count, size_t shift, size_t start) {
    arr[start] = temp_right;
 }
 
+
+// Name:    void tests()
+// Desc:    This function performs unit tests on shift_nums and shift_chain.
+// Input:   None
+// Output:  None
+// Return:  None; if a test fails, this function will abort the program.
 void tests() {
    int arr_chain[] = {1, 2, 3, 4, 5, 6};
    int arr_chain_expected[] = {3, 2, 5, 4, 1, 6};
@@ -178,7 +201,7 @@ void tests() {
       assert(arr[i] == arr_expected[i]);
    }
 
-   shift_nums(arr, 6, 4);
+   shift_nums(arr_weird, 6, 4);
    for (i = 0; i < 6; i++) {
       assert(arr_weird[i] == arr_weird_expected[i]);
    }
